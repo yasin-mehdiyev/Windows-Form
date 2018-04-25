ed1624d8239077ac2baab7a05efaf4fb350246d7
yasin-mehdiyev <30857286+yasin-mehdiyev@users.noreply.github.com>
2018-04-26 02:55:09 +0400

    Add files via upload

---
+++ Combo-Box/Combo-Box/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Combo_Box
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(49, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Marka";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(262, 105);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(246, 45);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(49, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 37);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Model";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.OliveDrab;
+            this.label3.Location = new System.Drawing.Point(49, 294);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 37);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Versiyasi";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(262, 183);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(246, 45);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(262, 286);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(246, 45);
+            this.comboBox3.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 37;
+            this.listBox1.Location = new System.Drawing.Point(56, 386);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(746, 263);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkRed;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(637, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 78);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Tomato;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(637, 253);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 78);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label4.Location = new System.Drawing.Point(162, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(568, 52);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Avtomobil Siyahisi Programi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 706);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Combo-Box/Combo-Box/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Combo_Box
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] marka = { "Audi", "Mersedes", "Toyota", "Suzuki" };
+            comboBox1.Items.AddRange(marka);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text=="Audi")
+            {
+                comboBox2.Items.Clear();
+                string[] model1 = { "A1", "A2", "A3"};
+                comboBox2.Items.AddRange(model1);
+            }
+            else if (comboBox1.Text == "Mersedes")
+            {
+                comboBox2.Items.Clear();
+                string[] model2 = { "B170", "B180", "B190" };
+                comboBox2.Items.AddRange(model2);
+            }
+            else if (comboBox1.Text == "Toyota")
+            {
+                comboBox2.Items.Clear();
+                string[] model3 = { "Carolla", "Camry", "Prado" };
+                comboBox2.Items.AddRange(model3);
+            }
+            else if (comboBox1.Text == "Suzuki")
+            {
+                comboBox2.Items.Clear();
+                string[] model4 = { "Alto", "Baleno", "Liana" };
+                comboBox2.Items.AddRange(model4);
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.Text=="A1")
+            {
+                comboBox3.Items.Clear();
+                string[] version1 = { "1.3", "1.5" };
+                comboBox3.Items.AddRange(version1);
+            }
+            else if (comboBox2.Text == "A2")
+            {
+                comboBox3.Items.Clear();
+                string[] version2 = { "1.5", "1.7","1.9" };
+                comboBox3.Items.AddRange(version2);
+            }
+            else if (comboBox2.Text == "A3")
+            {
+                comboBox3.Items.Clear();
+                string[] version3 = { "1.5", "1.7", "1.9","2.0" };
+                comboBox3.Items.AddRange(version3);
+            }
+            if (comboBox2.Text== "B170")
+            {
+                comboBox3.Items.Clear();
+                comboBox3.Items.Add(1.4);
+            }
+            else if (comboBox2.Text == "B180")
+            {
+                comboBox3.Items.Clear();
+                comboBox3.Items.Add(1.5);
+                comboBox3.Items.Add(1.8);
+            }
+            else if (comboBox2.Text == "B190")
+            {
+                comboBox3.Items.Clear();
+                comboBox3.Items.Add(1.3);
+            }
+            if (comboBox2.Text== "Carolla")
+            {
+                comboBox3.Items.Clear();
+                comboBox3.Items.Add(2.4);
+                comboBox3.Items.Add(2.7);
+            }
+            else if (comboBox2.Text == "Camry")
+            {
+                comboBox3.Items.Clear();
+                comboBox3.Items.Add(1.6);
+            }
+            else if (comboBox2.Text == "Prado")
+            {
+                comboBox3.Items.Clear();
+                comboBox3.Items.Add(1.2);
+                comboBox3.Items.Add(1.6);
+            }
+            if (comboBox2.Text == "Alto")
+            {
+                comboBox3.Items.Clear();
+                string[] version4 = { "2.5", "1.7", "2.9" };
+                comboBox3.Items.AddRange(version4);
+            }
+            else if (comboBox2.Text == "Baleno")
+            {
+                comboBox3.Items.Clear();
+                string[] version5 = { "3.2", "1.9", "2.2" };
+                comboBox3.Items.AddRange(version5);
+            }
+            else if (comboBox2.Text == "Liana")
+            {
+                comboBox3.Items.Clear();
+                string[] version6 = { "2.0", "2.3", "2.4" };
+                comboBox3.Items.AddRange(version6);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Marka - " + comboBox1.Text + " | Model - " + comboBox2.Text + " | Versiya - " + comboBox3.Text);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove("Marka - " + comboBox1.Text + " | Model - " + comboBox2.Text + " | Versiya - " + comboBox3.Text);
+        }
+    }
+}
