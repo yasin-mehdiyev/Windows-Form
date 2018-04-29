43d3807eee14bbe8e421f7f56df841803e8f87f1
yasin-mehdiyev <30857286+yasin-mehdiyev@users.noreply.github.com>
2018-04-29 19:18:38 +0400

    Add files via upload

---
+++ 28.04.2018/28.04.2018/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace _28._04._2018
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Name = "label5";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Name = "label4";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label7.Name = "label7";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Name = "label6";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Name = "label3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 28.04.2018/28.04.2018/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _28._04._2018
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {         
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {         
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text.Any(char.IsLower))
+            {
+                label3.ForeColor = Color.Silver;
+            }
+            else
+            {
+                label3.ForeColor = Color.Black;
+            }
+
+            if (textBox1.Text.Any(char.IsUpper))
+            {
+                label4.ForeColor = Color.Silver;
+            }
+            else
+            {
+                label4.ForeColor = Color.Black;
+            }
+
+
+            if (textBox1.Text.Any(char.IsNumber))
+            {
+                label5.ForeColor = Color.Silver;
+            }
+            else
+            {
+                label5.ForeColor = Color.Black;
+            }
+
+
+            if (textBox1.Text.Any(char.IsPunctuation))
+            {
+                label6.ForeColor = Color.Silver;
+            }
+            else
+            {
+                label6.ForeColor = Color.Black;
+            }
+
+
+            if (textBox1.Text.Length >= 8)
+            {
+                label7.ForeColor = Color.Silver;
+            }
+            else
+            {
+                label7.ForeColor = Color.Black;
+            }
+            if (label3.ForeColor == Color.Silver && label4.ForeColor == Color.Silver && label5.ForeColor == Color.Silver && label6.ForeColor == Color.Silver && label7.ForeColor == Color.Silver)
+            {
+                button1.Visible = true;
+                label3.Visible = false;
+                label4.Visible = false;
+                label5.Visible = false;
+                label6.Visible = false;
+                label7.Visible = false;
+                button1.Text = "Siz butun variantlari doldurmusunuz";
+            }
+            else
+            {
+                button1.Visible = false;
+                label3.Visible = true;
+                label4.Visible = true;
+                label5.Visible = true;
+                label6.Visible = true;
+                label7.Visible = true;
+            }
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+            if (label2.Text == "Show")
+            {
+                label2.Text = "Hide";
+                textBox1.UseSystemPasswordChar = true;
+            }
+            else
+            {
+                label2.Text = "Show";
+                textBox1.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+        }
+    }
+}
